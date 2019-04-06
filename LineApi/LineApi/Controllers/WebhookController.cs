efa85dcaaa8a9b4c2c25def93799ebf00a84b528
Hauser <free576002@gmail.com>
2019-04-06 12:05:29 +0800

    20190406

---
+++ LineApi/LineApi/Controllers/WebhookController.cs
@@ -0,0 +1,44 @@
+﻿using Line.Messaging;
+using Line.Messaging.Webhooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace LineApi.Controllers
+{
+    [Route("api")]
+    public class WebhookController : ApiController
+    {
+        private static LineMessagingClient lineMessagingClient;
+        private string AccessToken = "Your channel Token";
+        private string ChannelSecret = "ChannelSecret";
+        public WebhookController()
+        {
+            lineMessagingClient = new LineMessagingClient(AccessToken);
+        }
+
+
+
+        [Route("api/alive")]
+        [HttpGet]
+        public string Get()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        [Route("api/lineapp/")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post(HttpRequestMessage request)
+        {
+            var @events = await request.GetWebhookEventsAsync(ChannelSecret);
+
+            var App = new LineBotApp(lineMessagingClient);
+            await App.RunAsync(@events);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ LineApi/LineApi/LineBotApp.cs
@@ -0,0 +1,57 @@
+﻿using Line.Messaging;
+using Line.Messaging.Webhooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LineApi
+{
+    internal class LineBotApp : WebhookApplication
+    {
+        private LineMessagingClient MessagingClient { get; }
+
+        public LineBotApp(LineMessagingClient _LineMessagingClient)
+        {
+            this.MessagingClient = _LineMessagingClient;
+        }
+
+        protected override async Task OnMessageAsync(MessageEvent ev)
+        {
+            switch (ev.Message.Type)
+            {
+                case EventMessageType.Text:
+                    await HandleTextAsync(ev.ReplyToken, ((TextEventMessage)ev.Message).Text, ev.Source.UserId);
+                    break;
+
+                case EventMessageType.Image:
+                case EventMessageType.Audio:
+                case EventMessageType.Video:
+                case EventMessageType.File:
+                case EventMessageType.Location:
+                case EventMessageType.Sticker:
+                    break;
+            }
+        }
+
+
+        private async Task HandleTextAsync(string ReplyToken, string UserMessage, string UserId)
+        {
+            List<ISendMessage> IReplyMessage = new List<ISendMessage>();
+            var Hellosticks = new StickerMessage[] {
+                            new StickerMessage("1", "2"), new StickerMessage("1", "134"), new StickerMessage("2", "143") ,
+                            new StickerMessage("2", "34"), new StickerMessage("1", "407"), new StickerMessage("1", "106")
+                        };
+
+            var ReplyMessage = new TextMessage($"這是你的UserId ： {UserId}");
+            var Sticks = Hellosticks[new Random().Next(Hellosticks.Length - 1)];
+
+
+            IReplyMessage.Add(ReplyMessage);
+            IReplyMessage.Add(Sticks);
+
+            await MessagingClient.ReplyMessageAsync(ReplyToken, IReplyMessage);
+        }
+    }
+}
